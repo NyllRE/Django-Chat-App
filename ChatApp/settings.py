07c0b2ff7291d5797445164e1d6ecf4c6ff02fe7
NyllRE <alix69gamer@gmail.com>
2021-01-26 01:11:25 +0300

    static files deployment settings

--- ChatApp/settings.py
+++ ChatApp/settings.py
@@ -124,7 +124,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/var/www/static/',
+]
 
 ASGI_APPLICATION = 'ChatApp.routing.application'
 CHANNEL_LAYERS = {
