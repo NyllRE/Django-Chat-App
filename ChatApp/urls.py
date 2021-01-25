28fc54c100381adf69c22bad59b77b3e74276563
NyllRE <alix69gamer@gmail.com>
2021-01-26 01:24:52 +0300

    static files deployment settings

--- ChatApp/urls.py
+++ ChatApp/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from users import views as user_views
 from chat.views import home
 from django.contrib.auth import views as auth_views
+from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
